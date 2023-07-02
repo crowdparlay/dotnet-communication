5f7cec64295debac26a73dd8dc7a00970e91ee83
undrcrxwn <undrcrxwn@gmail.com>
2023-07-02 22:36:34 +0300

    feat(destination): improve exchange subscription API

--- src/CrowdParlay.Communication.RabbitMq/RabbitMqExchange.cs
+++ src/CrowdParlay.Communication.RabbitMq/RabbitMqExchange.cs
@@ -28,7 +28,7 @@ public class RabbitMqExchange : IMessageDestination, IDisposable
     public void Publish(Message message)
     {
         _channel.ExchangeDeclare(_exchange, ExchangeType.Topic);
-        
+
         var json = JsonConvert.SerializeObject(message);
         var body = Encoding.UTF8.GetBytes(json);
         var routingKey = ResolveRoutingKey(message.GetType());
@@ -36,27 +36,36 @@ public class RabbitMqExchange : IMessageDestination, IDisposable
         _channel.BasicPublish(_exchange, routingKey, body: body);
     }
 
-    public void Subscribe<TMessage>(IMessageListener<TMessage> listener) where TMessage : Message
+    public void Subscribe<TMessage>(IMessageListener<TMessage> listener) where TMessage : Message =>
+        Subscribe<TMessage>(async message => await listener.HandleAsync(message));
+    
+    public void Subscribe<TMessage>(Func<TMessage, Task> handleMessage) where TMessage : Message =>
+        Subscribe(typeof(TMessage), async message => await handleMessage((TMessage)message));
+    
+    public void Subscribe(Type messageType, Func<Message, Task> handleMessage)
     {
-        _channel.ExchangeDeclare(_exchange, ExchangeType.Topic);
+        if (!messageType.IsAssignableTo(typeof(Message)))
+            throw new ArgumentException($"Only types derived from '{typeof(Message)}' can be used as message types.", nameof(messageType));
         
-        var routingKey = ResolveRoutingKey(typeof(TMessage));
+        _channel.ExchangeDeclare(_exchange, ExchangeType.Topic);
+
+        var routingKey = ResolveRoutingKey(messageType);
         var queue = _channel.QueueDeclare().QueueName;
         _channel.QueueBind(queue, _exchange, routingKey);
-        
+
         var consumer = new EventingBasicConsumer(_channel);
         consumer.Received += async (_, args) =>
         {
-            _channel.BasicAck(args.DeliveryTag, multiple: false);
-
             var body = Encoding.UTF8.GetString(args.Body.ToArray());
-            var message = JsonConvert.DeserializeObject<TMessage>(body)!;
-            await listener.HandleAsync(message);
+            var message = (Message)JsonConvert.DeserializeObject(body, messageType)!;
+            await handleMessage(message);
+            
+            _channel.BasicAck(args.DeliveryTag, multiple: false);
         };
 
         _channel.BasicConsume(queue, autoAck: false, consumer);
     }
-
+    
     private string ResolveRoutingKey(Type messageType)
     {
         if (!_routingKeysByMessageType.TryGetValue(messageType, out var routingKey))
--- src/CrowdParlay.Communication/Abstractions/IMessageDestination.cs
+++ src/CrowdParlay.Communication/Abstractions/IMessageDestination.cs
@@ -4,4 +4,6 @@ public interface IMessageDestination
 {
     public void Publish(Message message);
     public void Subscribe<TMessage>(IMessageListener<TMessage> listener) where TMessage : Message;
+    public void Subscribe<TMessage>(Func<TMessage, Task> handleMessage) where TMessage : Message;
+    public void Subscribe(Type messageType, Func<Message, Task> handleMessage);
 }
