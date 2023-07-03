c1adb56a6ff71ef764cda4c7bce80afc84a2600a
undrcrxwn <undrcrxwn@gmail.com>
2023-07-03 21:49:14 +0300

    fix(listener-initializer): replace scoped broker injection with scope factory injection in listener initializer hosted service

--- src/CrowdParlay.Communication.RabbitMq.DependencyInjection/MessageListenersInitializer.cs
+++ src/CrowdParlay.Communication.RabbitMq.DependencyInjection/MessageListenersInitializer.cs
@@ -1,4 +1,5 @@
 using CrowdParlay.Communication.Abstractions;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 namespace CrowdParlay.Communication.RabbitMq.DependencyInjection;
@@ -6,21 +7,24 @@ namespace CrowdParlay.Communication.RabbitMq.DependencyInjection;
 public sealed class MessageListenersInitializer : IHostedService
 {
     private readonly MessageListenersConfiguration _configuration;
-    private readonly IMessageBroker _broker;
     private readonly IMessageListener<Message> _commonListener;
+    private readonly IServiceScopeFactory _scopeFactory;
 
     public MessageListenersInitializer(
-        MessageListenersConfiguration configuration, IMessageBroker broker, IMessageListener<Message> commonListener)
+        MessageListenersConfiguration configuration, IMessageListener<Message> commonListener, IServiceScopeFactory scopeFactory)
     {
         _configuration = configuration;
-        _broker = broker;
         _commonListener = commonListener;
+        _scopeFactory = scopeFactory;
     }
 
     public Task StartAsync(CancellationToken cancellationToken)
     {
+        using var scope = _scopeFactory.CreateScope();
+        var broker = scope.ServiceProvider.GetRequiredService<IMessageBroker>();
+        
         foreach (var messageType in _configuration.SubscribedMessageTypes)
-            _broker.Users.Subscribe(messageType, _commonListener.HandleAsync);
+            broker.Users.Subscribe(messageType, _commonListener.HandleAsync);
 
         return Task.CompletedTask;
     }
