2248778a348bcdf4ce83e15bb65fcc98e316c82f
undrcrxwn <undrcrxwn@gmail.com>
2023-07-13 13:57:46 +0300

    feat: add AvatarUrl property to user events

--- src/CrowdParlay.Communication/Messages.cs
+++ src/CrowdParlay.Communication/Messages.cs
@@ -2,6 +2,6 @@ using CrowdParlay.Communication.Abstractions;
 
 namespace CrowdParlay.Communication;
 
-public record UserCreatedEvent(string UserId, string Username, string DisplayName) : Message;
-public record UserUpdatedEvent(string UserId, string Username, string DisplayName) : Message;
+public record UserCreatedEvent(string UserId, string Username, string DisplayName, string? AvatarUrl) : Message;
+public record UserUpdatedEvent(string UserId, string Username, string DisplayName, string? AvatarUrl) : Message;
 public record UserDeletedEvent(string UserId) : Message;
